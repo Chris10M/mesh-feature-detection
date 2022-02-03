54b4c520f5246de1e886f8019326782f47d03fd9
Christen M <mrkristen@gmail.com>
2022-02-03 18:05:03 +0100

    guassian curvature working

--- get_principal_curvatures.m
+++ get_principal_curvatures.m
@@ -1,8 +1,10 @@
 function [K_1, K_2] = get_principal_curvatures(K_H, K_G)    
-    % zeros = zeros(numv, 1)
-    % delx = np.sqrt(np.max(np.vstack((K_H**2 - K_G, zeros)), axis=0))
-    % K_1 = K_H + delx
-    % K_2 = K_H - delx
+	delx = K_H .* K_H - K_G;
+	
+    delx(find(delx < 0)) = 0;
 
+    delx_sqrt = power(delx, 0.5);
 
+	K_1 = K_H + delx_sqrt;
+	K_2 = K_H - delx_sqrt;
 end
